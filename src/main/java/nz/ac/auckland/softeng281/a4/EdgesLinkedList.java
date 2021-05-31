e548fd50b53cd1eb3538d57fd27dcdcafc5f6887
Ananya <aahl907@aucklanduni.ac.nz>
2021-05-31 15:16:15 +1200

    Implemented get() and size() methods - TEST 1 PASSES

--- src/main/java/nz/ac/auckland/softeng281/a4/EdgesLinkedList.java
+++ src/main/java/nz/ac/auckland/softeng281/a4/EdgesLinkedList.java
@@ -7,104 +7,118 @@ package nz.ac.auckland.softeng281.a4;
  * @author Partha Roop
  */
 public class EdgesLinkedList {
-    // the head of the linked list
-    private Edge head;
+	// the head of the linked list
+	private Edge head;
 
-    public EdgesLinkedList() {
-        head = null;
-    }
+	public EdgesLinkedList() {
+		head = null;
+	}
 
 
-    /**
-     * This method adds a node with specified data as the start node of the list
-     *
-     * @param : an integer, which is the value of the Node
-     * @return void
-     */
-    public void prepend(Edge e) {
-    	Edge n = new Edge(e.getSource(), e.getTarget(), e.getWeight());
+	/**
+	 * This method adds a node with specified data as the start node of the list
+	 *
+	 * @param : an integer, which is the value of the Node
+	 * @return void
+	 */
+	public void prepend(Edge e) {
+		Edge n = new Edge(e.getSource(), e.getTarget(), e.getWeight());
 		n.setNext(head);
 		head = n;
+
+	}
+
+	/**
+	 * This method adds a node with specified data as the end node of the list
+	 *
+	 * @param : an integer, which is the value of the Node
+	 * @return void
+	 */
+
+	public void append(Edge edge) {
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
+
+	/**
+	 * This method gets the value of a node at a given position
+	 *
+	 * @param pos: an integer, which is the position
+	 * @return the value at the position pos
+	 */
+
+	public Edge get(int pos) throws InvalidPositionException {
+		if (pos < 0 || pos > size() - 1) {
+			throw new InvalidPositionException("Position " + pos + " outside the list boundary");
+		}
+		int i = 0;
+		Edge t = head;
+		while(i!=pos) {
+			++i;
+			t=t.getNext();
+		}
 		
-    }
+		return t;
+	}
 
-    /**
-     * This method adds a node with specified data as the end node of the list
-     *
-     * @param : an integer, which is the value of the Node
-     * @return void
-     */
+	/**
+	 * This method fetches the value of a node at a given position
+	 *
+	 * @param pos: an integer, which is the position
+	 * @return the value at the position pos
+	 */
 
-    public void append(Edge edge) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-    }
+	public void insert(int pos, Edge edge) throws InvalidPositionException {
+		if (pos < 0 || pos > size() - 1) {
+			throw new InvalidPositionException("Position " + pos + " outside the list boundary");
+		}
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
 
-    /**
-     * This method gets the value of a node at a given position
-     *
-     * @param pos: an integer, which is the position
-     * @return the value at the position pos
-     */
+	/**
+	 * This method removes a node at a given position
+	 *
+	 * @param pos: an integer, which is the position
+	 * @return void
+	 */
 
-    public Edge get(int pos) throws InvalidPositionException {
-        if (pos < 0 || pos > size() - 1) {
-            throw new InvalidPositionException("Position " + pos + " outside the list boundary");
-        }
-        throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-    }
+	public void remove(int pos) throws InvalidPositionException {
+		if (pos < 0 || pos > size() - 1) {
+			throw new InvalidPositionException("Position " + pos + " outside the list boundary");
+		}
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
 
-    /**
-     * This method fetches the value of a node at a given position
-     *
-     * @param pos: an integer, which is the position
-     * @return the value at the position pos
-     */
+	/**
+	 * This method returns the size of a list
+	 *
+	 * @param
+	 * @return the size of the list
+	 */
 
-    public void insert(int pos, Edge edge) throws InvalidPositionException {
-        if (pos < 0 || pos > size() - 1) {
-            throw new InvalidPositionException("Position " + pos + " outside the list boundary");
-        }
-        throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-    }
+	public int size() {
+		int i = 0;
 
-    /**
-     * This method removes a node at a given position
-     *
-     * @param pos: an integer, which is the position
-     * @return void
-     */
+		Edge t = head;
+		while(t!=null) {
+			++i;
+			t=t.getNext();
+		}
+		return i;
+	}
 
-    public void remove(int pos) throws InvalidPositionException {
-        if (pos < 0 || pos > size() - 1) {
-            throw new InvalidPositionException("Position " + pos + " outside the list boundary");
-        }
-        throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-    }
+	/**
+	 * This method is used for printing the data in the list from head till the last
+	 * node
+	 *
+	 * @param
+	 * @return void
+	 */
 
-    /**
-     * This method returns the size of a list
-     *
-     * @param
-     * @return the size of the list
-     */
-
-    public int size() {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-    }
-
-    /**
-     * This method is used for printing the data in the list from head till the last
-     * node
-     *
-     * @param
-     * @return void
-     */
-
-    public void print() {
-        Edge edge = head;
-        while (edge != null) {
-            System.out.println(edge);
-            edge = edge.getNext();
-        }
-    }
+	public void print() {
+		Edge edge = head;
+		while (edge != null) {
+			System.out.println(edge);
+			edge = edge.getNext();
+		}
+	}
 }
